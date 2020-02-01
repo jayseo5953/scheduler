3dbb7761f67ec95a52865a4e49b44589d6a026b6
Jay Seo <sjs5953@hotmail.com>
2020-01-31 16:40:51 -0800

    test

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -74,6 +74,7 @@ export default function Appointment(props) {
     }
   }, [props.interview, mode, transition]);
 
+  
   return (
     <article data-testid="appointment" className="appointment">
       <Header time={props.time} />
